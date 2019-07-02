4db199ad40f88ec476198a889e7cf9196d5a1688
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-07-02 11:57:21 -0500

    Update export_bbsFeathers.R

--- R/export_bbsFeathers.R
+++ R/export_bbsFeathers.R
@@ -1,5 +1,5 @@
 #' @title Save BBS dataframe as a feather file on disk. 
-#' @description Save the BBS data frame as a feather file to disk for easier import. 
+#' @description Save the BBS data frame as a feather file to disk for easier import.
 #' @param dataIn The BBS data to save.
 #' @param newDir Where to save the BBS data as feathers.
 #' @param filename Name of the new filename (e.g., 'arizona.zip'). This function will replace .zip with .feather
@@ -7,6 +7,7 @@
 
 export_bbsFeathers <- function(dataIn, newDir) {
     
+
     # Create the new filename as .feather
     filename = gsub(".zip" , ".feather", filename)
     
@@ -15,7 +16,7 @@ export_bbsFeathers <- function(dataIn, newDir) {
         dplyr::select(year, countrynum, statenum, route, bcr, latitude, longitude,
                       aou, stoptotal)
     
-    # WRite to disk
+    # Write to disk
     write_feather(x = temp, path = paste0(newDir, filename))
     
     
