7f0bf564d6ed8270f5e01554d010be6ab5594cdd
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-07-02 16:06:53 -0500

    Successful build + runthrough.RMD

--- tests/testthat/test-get_credibility-and-trends.R
+++ tests/testthat/test-get_credibility-and-trends.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
+library(rvest)
+library(stringr)
+library(tidyverse)
+library(gdata)
+
+
+test_that("Check credibility import is correct ", {
   expect_equal(2 * 2, 4)
+})
+
+test_that("Check species trend estimates import is correct ", {
+    expect_equal(2 * 2, 4)
 })
