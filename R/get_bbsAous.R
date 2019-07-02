94b358b70149b58afaeb2f7fa0ec0be3418e5772
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-07-02 11:53:52 -0500

    Build + manual
    Added folder for devnotes -- here we can follow these producures when building pkg.

--- R/get_bbsAous.R
+++ R/get_bbsAous.R
@@ -1,5 +1,5 @@
 #' @title Import the species list used by the BBS from website or local disk.
-#' @description This function imports the species list (incuding AOU numbers) from their website. This list can be used to subet the species by name, AOU number, etc. 
+#' @description This function imports the species list (including AOU numbers) from their website. This list can be used to subet the species by name, AOU number, etc. 
 #' @param file A character string for location for the species list (as .txt).
 #' @param skipEmpty Numeric, default = 7. The number of lines to skip. This may need to be updated if the file on webpage changes. 
 #' 
