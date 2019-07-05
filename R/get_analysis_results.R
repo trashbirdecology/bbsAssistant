80c4a2789f284aad1cb68fac3eef8161a66918b0
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-07-05 13:04:39 -0500

    Add get_analysis_results.R function
    This function allows user to downlaod the .csv containing results as opposed to using get_crediblity_Trends.r

--- R/get_analysis_results.R
+++ R/get_analysis_results.R
@@ -4,13 +4,13 @@
 #' @param url Web link to the .csv file for annual abundance indices. Default = NULL.
 #' @export get_ananlysis_results
 
-get_ananlysis_results <-
+get_analysis_results <-
     function(url = NULL, 
              analysis = "trend.ests"
              ) {
         
         if(!analysis %in% c("annual.inds.2016", "trend.ests","core.trend.revised.2017",
-                            "core.2017","expanded.2017","core.twedt.revised.2017"))
+                            "core.2017","expanded.2017","core.twedt.revised.2017")) error("Please specify arg 'analysis'")
         
         if(analysis=="trend.ests") url <- "https://www.mbr-pwrc.usgs.gov/bbs/BBS_Trend_Estimates_2015_7-29-2016.csv"
         if(analysis=="annual.inds.2016") url <- "https://www.mbr-pwrc.usgs.gov/bbs/BBS_Annual_Indices_Estimates_2015_7-29-2016.csv"
@@ -18,6 +18,7 @@ get_ananlysis_results <-
         if(analysis=="core.2017") url <- "https://www.mbr-pwrc.usgs.gov/bbs/inde_1966-2017_core_v2.csv"
         if(analysis=="expanded.2017") url <- "https://www.mbr-pwrc.usgs.gov/bbs/inde_1993-2017_expanded.csv"
         if(analysis=="core.twedt.revised.2017") url <- "https://www.mbr-pwrc.usgs.gov/bbs/BBS_1966-2017_core_twedt_trend_revised.csv"
+        
         
         df  <- read.csv(url)
         
