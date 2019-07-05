dd5aa4e0acd3e0bc03db428d90faba3f2773af87
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-07-05 16:20:48 -0500

    Add exports, update .ignore

--- R/get_analysis_results.R
+++ R/get_analysis_results.R
@@ -2,7 +2,7 @@
 #' @description Downloads all species-regions annual abundances indices from various .csv files on the BBS analysis results page. See <https://www.mbr-pwrc.usgs.gov/bbs/BBSAnalysis_Results_Metadata_1966_2017_v3.xml> for metadata for analysis results and trend estiamtes. 
 #' @param analysis Default = "trend.ests". Must be one of :c("annual.inds.2016", "trend.ests","core.trend.revised.2017", "core.2017","expanded.2017","core.twedt.revised.2017")
 #' @param url Web link to the .csv file for annual abundance indices. Default = NULL.
-#' @export get_ananlysis_results
+#' @export get_analysis_results
 
 get_analysis_results <-
     function(url = NULL, 
