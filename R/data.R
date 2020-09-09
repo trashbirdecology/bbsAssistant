dc7a79ddfd4c6171bcf9866a50be10c78ee4f971
trashbirdecology <jessicaleighburnett@gmail.com>
2020-09-09 13:53:37 -0600

    Add script for handling region_codes
    Efficiently pulling out of the XML metadata file components which describe values in StateNum and CountryNum is a total PITA for JLB at this moment in time.
    
    Currently, this commit just ensures we have the original, "handmade" region code lookup table (data-raw/region_code.csv) and documentation.

--- R/data.R
+++ R/data.R
@@ -1,3 +1,36 @@
+# region_codes ------------------------------------------------------------
+#' Country and state codes and associated zip filenames 
+#' @format A data frame:
+#' \describe{
+#'   \item{CountryNum}{Integer. BBS country codes associated with United States-840, Mexico-484, and Canada-124}
+#'   \item{StateNum}{Integer. BBS state numbers associated with each 'StateNum' and 'CountryNum'}
+#'   \item{State}{Character. The proper name of the US or Mexican state or Canadian province.}
+#'   \item{zip_states}{Character. The filename of the .zip compressed BBS count data located on the FTP server. These files do not exist for Mexican states, so == NA}
+#' }
+#' @source \url{https://www.sciencebase.gov/catalog/item/52b1dfa8e4b0d9b325230cd9}
+"region_codes"
+
+# bbs_recent  (most recent version of bbs observations) --------------------------------------------------------------
+#' Contains the most recent release of the BBS observations dataset. 
+#' @format A data frame with 6797797 rows and 14 columns (as of 2020-09-09).
+#' \describe{
+#'   \item{RouteDataID}{}
+#'   \item{CountryNum}{BBS country codes associated with United States-840, Mexico-484, and Canada-124}
+#'   \item{StateNum}{BBS state numbers associated with each 'State' and 'CountryNum'}
+#'   \item{State}{The proper name of the US or Mexican state or Canadian province}
+#'   \item{RPID}{Route run type}
+#'   \item{Year}{Year in which the BBS observations was taken.}
+#'   \item{AOU}{Numeric AOU code.}-0 
+#'   \item{StopTotal}{}
+#'   \item{Count10}{}
+#'   \item{Count20}{}
+#'   \item{Count30}{}
+#'   \item{Count40}{}
+#'   \item{Count50}{}
+#' }
+#' @source \url{https://www.sciencebase.gov/catalog/item/52b1dfa8e4b0d9b325230cd9}
+"bbs_recent"
+
 # trend_ests_core_1966to2017 ----------------------------------------------
 #' Trend estimates for the core BBS area, 1966-2017
 #' @format A data frame:
@@ -48,37 +81,4 @@
 #' }
 #' @source \url{https://www.mbr-pwrc.usgs.gov/bbs/inde_1966-2017_core_v2.csv}
 "annual_index_core_1966to2017"
-
-# region_codes ------------------------------------------------------------
-#' Country and state codes and associated zip filenames 
-#' @format A data frame:
-#' \describe{
-#'   \item{CountryNum}{BBS country codes associated with United States-840, Mexico-484, and Canada-124}
-#'   \item{StateNum}{BBS state numbers associated with each 'State' and 'CountryNum'}
-#'   \item{State}{The proper name of the US or Mexican state or Canadian province}
-#'   \item{zip_states}{The filename of the .zip compressed BBS count data located on the FTP server. These files do not exist for Mexican states, so == NA}
-#' }
-#' @source \url{ftp://ftpext.usgs.gov/pub/er/md/laurel/BBS/DataFiles/RegionCodes.txt}
-"region_codes"
-
-# bbs_recent  (most recent version of bbs observations) --------------------------------------------------------------
-#' Contains the most recent release of the BBS observations dataset. 
-#' @format A data frame with 6797797 rows and 14 columns (as of 2020-09-09).
-#' \describe{
-#'   \item{RouteDataID}{}
-#'   \item{CountryNum}{BBS country codes associated with United States-840, Mexico-484, and Canada-124}
-#'   \item{StateNum}{BBS state numbers associated with each 'State' and 'CountryNum'}
-#'   \item{State}{The proper name of the US or Mexican state or Canadian province}
-#'   \item{RPID}{Route run type}
-#'   \item{Year}{Year in which the BBS observations was taken.}
-#'   \item{AOU}{Numeric AOU code.}-0 
-#'   \item{StopTotal}{}
-#'   \item{Count10}{}
-#'   \item{Count20}{}
-#'   \item{Count30}{}
-#'   \item{Count40}{}
-#'   \item{Count50}{}
-#' }
-#' @source \url{https://www.sciencebase.gov/catalog/item/52b1dfa8e4b0d9b325230cd9}
-"bbs_recent"
 
