c044ec7e40d48dec888001210922e97726406b34
trashbirdecology <jessicaleighburnett@gmail.com>
2020-09-09 17:06:22 -0600

    Add more import utils
    Simple funs for reading in components of the BBS observations dataset

--- R/import_bbs_data.R
+++ R/import_bbs_data.R
@@ -1,23 +1,21 @@
-#' @title Read BBS data from local file into R environment.
-#' @description 
-#' @param sb_dir
-#' @param state
-#' @param country
-#' 
-#' 
-import_bbs_data <- function(sb_dir, state=NULL, country=NULL){
+#' @title Read BBS data from local file into R environment. 
+#' @description  Called inside `get_bbs_data()` but can be used independently.
+#' @param sb_dir Directory location for the ScienceBase item.
+#' @param state Optional. Imports certain files based on producer-supplied state/province codes or English names.
+#' @param country Optional. Imports certain files based on producer-supplied country/nation codes or English names.
+
+import_bbs_data <- function(sb_dir, state, country){
     
 # Get list of the available obesrvation filenames -----------------------------------------------------------
 state.fns <- list.files(paste0(sb_dir, "/States/"), pattern=".csv", full.names=TRUE)
 if(length(state.fns)==0){stop("No State-level csv files were found in ", sb_dir, "/States/", "./n/n", "Please ensure the data were downloaded and unpacked in the correctly location. Sorry :(")}
 
 # Subset filenames by params state, country -------------------------------
-##NEED TO ADD THIS FUNCITONALITY...
-# state.fns %>% filter()    
+## NEED TO ADD THIS FUNCITONALITY...
+# i.e., state.fns %>% filter()    
     
-
-# Load desired files into environment as a single dataframe -------------------------------------
-state.data <- NULL
+# Load desired observation files (per state) into environment as a single dataframe -------------------------------------
+observations <- NULL
 for(i in seq_along(state.fns)){
     message("Attempting to import file ", state.fns[i], " (File ",i," of ", length(state.fns), ")")
     suppressMessages( temp <- readr::read_csv(state.fns[i]), classes = ) # suppress all the messages about column types...bleh need to resolve this
@@ -25,16 +23,24 @@ for(i in seq_along(state.fns)){
         dplyr::mutate(StateNum = as.integer(StateNum), 
                CountryNum = as.integer(CountryNum) 
         )
-    state.data <- dplyr::bind_rows(temp, state.data)
-    
+    observations <- dplyr::bind_rows(temp, state.data)
     }
 
-# Join state.data with observation data with route metadata -------------------------------
+# Load the route level metadata for this dataset release -------------------------------
 routes  <-  suppressMessages(import_routes(sb_dir))
+weather <-  suppressMessages(import_weather(sb_dir))
+# unique(routes$StateNum) %in% unique(state.data$StateNum) # THIS SHOULD BE A TEST!
+# unique(state.data$RouteDataID) %>% length() # should also be a test....
 
-names(routes)
-names(state.data)
+
+# Get dataset citation(s) -------------------------------------------------
+citation <- sbtools::item_get_fields(sb_id, "citation")
+
+# Create a list of data and information to export or return to `get_bbs_data` ----------------------------------
+bbs <- list(observations, routes, weather, citation)
+names(bbs) <- c("observations", "routes", "weather", "citation")
+
 
 # END FUNCTION ------------------------------------------------------------
-return(bbs_data)
+return(bbs)
 }
---
+++ R/import_weather.R
@@ -0,0 +1,13 @@
+#' @description Unzips and imports the Weather.zip (into weather.csv) file associated with each BBS dataset. 
+#' @param sb_dir Directory for the ScienceBase (sb) item.
+
+import_weather <- function(sb_dir){
+    # Unzip the States.zip into the SB item directory  (sb_dir) 
+    unzip(list.files(sb_dir, full.names=TRUE, pattern="eather.zip"), exdir = sb_dir)
+    
+    weather <- readr::read_csv(list.files(sb_dir, full.names=TRUE, pattern="weather.csv")) %>% 
+        dplyr::mutate(StateNum = as.integer(StateNum),
+                      CountryNum = as.integer(CountryNum))
+    
+    return(weather)
+}
