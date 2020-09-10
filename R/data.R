c85dcfd7592c2a31170421234ffbe09d63d2a0e0
trashbirdecology <jessicaleighburnett@gmail.com>
2020-09-09 21:03:05 -0600

    Updating docs - minor shit for cran

--- R/data.R
+++ R/data.R
@@ -1,8 +1,6 @@
-
-
 # sb_items ------------------------------------------------------------
 #' A lookup table that is manually updated by the package maintainers. It is used as a quick reference to identify which datasets are available for import from ScienceBase, and are compatible with this package.  
-#' @format A data frame:
+#' @format A data frame containing N rows and 5 columns
 #' \describe{
 #'   \item{sb_parent}{ScienceBase unique identifier associated with the parent item (often the project identifier).}
 #'   \item{sb_item}{ScienceBase unique identifier associated with each dataset release.}
@@ -31,19 +29,21 @@
 #' Contains the most recent release of the BBS observations dataset. 
 #' @format A data frame with 6797797 rows and 14 columns (as of 2020-09-09).
 #' \describe{
-#'   \item{RouteDataID}{}
+#'   \item{AOU}{Numeric AOU code.}
+#'   \item{Count10}{...}
+#'   \item{Count20}{...}
+#'   \item{Count30}{...}
+#'   \item{Count40}{...}
+#'   \item{Count50}{...}
 #'   \item{CountryNum}{BBS country codes associated with United States-840, Mexico-484, and Canada-124}
-#'   \item{StateNum}{BBS state numbers associated with each 'State' and 'CountryNum'}
-#'   \item{State}{The proper name of the US or Mexican state or Canadian province}
+#'   \item{Route}{...}
+#'   \item{RouteDataID}{...}
 #'   \item{RPID}{Route run type}
+#'   \item{SpeciesTotal}{...}
+#'   \item{State}{The proper name of the US or Mexican state or Canadian province}
+#'   \item{StateNum}{BBS state numbers associated with each 'State' and 'CountryNum'}
+#'   \item{StopTotal}{Number of individuals of a given species ('AOU') identified by the observer at the end of every 10th stop within the route.}
 #'   \item{Year}{Year in which the BBS observations was taken.}
-#'   \item{AOU}{Numeric AOU code.}-0 
-#'   \item{StopTotal}{}
-#'   \item{Count10}{}
-#'   \item{Count20}{}
-#'   \item{Count30}{}
-#'   \item{Count40}{}
-#'   \item{Count50}{}
 #' }
 #' @source \url{https://www.sciencebase.gov/catalog/item/52b1dfa8e4b0d9b325230cd9}
 "bbs_obs"
