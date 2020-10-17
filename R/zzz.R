293e229b7a321ca10ce2c5cdfdeaafb38c61188a
trashbirdecology <jessicaleighburnett@gmail.com>
2020-10-17 09:11:41 -0600

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -6,15 +6,16 @@
 #'
 #' @return invisible()
 .onAttach <- function(libname, pkgname) {
-    start_message <- c( "TERMS OF USE: North American Breeding Bird Survey Data:\n\n"
-                        , "Users of these BBS data are obligated to formally recognize their use of the program's\n"
-                        , "data in publications, presentations and other outlets. Additionally, all work using these data\n"
-                        , "should acknowledge the thousands of U.S. and Canadian participants who annually perform \n"
-                        , "and coordinate the survey. It is in the best interest for the continued success of\n"
-                        , "the BBS that authors submit a reprint or pdf of their work featuring BBS data\n"
-                        , "to the National BBS staff for inclusion in the program's bibliography.\n"
-                        , "If a publication is based solely on the analysis of BBS data, we recommend\n"
-                        , "that you involve National BBS staff with the writing and/or review of the manuscript.\n\n\n"
+    start_message <- c( "TERMS OF USE: North American Breeding Bird Survey Data:\n\n", 
+                        "Users of these BBS data are obligated to formally recognize their use of the program's
+data in publications, presentations and other outlets. Additionally, all work using these data
+should acknowledge the thousands of U.S. and Canadian participants who annually perform 
+and coordinate the survey. It is in the best interest for the continued success of
+the BBS that authors submit a reprint or pdf of their work featuring BBS data
+to the National BBS staff for inclusion in the program's bibliography.
+If a publication is based solely on the analysis of BBS data, we recommend
+that you involve National BBS staff with the writing and/or review of the manuscript.",
+                        
                         ,"DATA LIABILITY DISCLAIMER: North American Breeding Bird Survey Data"
                         , "This database, identified as the North American Breeding Bird Survey Dataset," 
                         , "has been approved for release and publication by the U.S. Geological Survey (USGS)" 
