4bf4505a21008b74263c942dac3d444abc2ef581
trashbirdecology <jessicaleighburnett@gmail.com>
2020-10-17 18:51:07 -0600

    styling mostly

--- R/import_bbs_data.R
+++ R/import_bbs_data.R
@@ -7,8 +7,8 @@
 #' @export import_bbs_data
 
 import_bbs_data <- function(sb_id, sb_dir, state=NULL, country=NULL){
-    
-# Get list of the available obesrvation filenames -----------------------------------------------------------
+
+# Get list of the available observation filenames -----------------------------------------------------------
 state.fns <- list.files(paste0(sb_dir, "/States/"), pattern=".csv", full.names=TRUE)
 if(length(state.fns)==0){stop("No State-level csv files were found in ", sb_dir, "/States/", "./n/n", "Please ensure the data were downloaded and unpacked in the correctly location. Sorry :(")}
 
@@ -29,23 +29,28 @@ for(i in seq_along(state.fns)){
     }
 
 # Load the route level metadata for this dataset release -------------------------------
-routes  <-  suppressMessages(import_routes(sb_dir))
-weather <-  suppressMessages(import_weather(sb_dir))
+routes  <-  suppressMessages(bbsAssistant::import_routes(sb_dir))
+weather <-  suppressMessages(bbsAssistant::import_weather(sb_dir))
 # unique(routes$StateNum) %in% unique(state.data$StateNum) # THIS SHOULD BE A TEST!
 # unique(state.data$RouteDataID) %>% length() # should also be a test....
 
+species_list <- bbsAssistant::species_list
 
 # Get dataset citation(s) -------------------------------------------------
 citation <- sbtools::item_get_fields(sb_id, "citation")
-
 
 # Get species list (Currently abides to AOU standards per BBS metadata) --------------------------------------------------
 # data(species_list, package="bbsAssistant")
 bbs_obs <- bbsAssistant::bbs_obs
 
-# Create a list of data and information to export or return to `get_bbs_data` ----------------------------------
+# Create a list of data and information to export or return to `get_bbs_data`----------------------------------dfnames <-
 bbs <- list(observations, routes, weather, citation, species_list)
-names(bbs) <- c("observations", "routes", "weather", "citation", "species_list")
+names(bbs) <-
+    c("observations",
+      "routes",
+      "weather",
+      "citation",
+      "species_list")
 
 # END FUNCTION ------------------------------------------------------------
 return(bbs)
