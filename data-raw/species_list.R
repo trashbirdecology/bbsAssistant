057d8100e5e0410168ac70ef4f93ed94aed0797f
trashbirdecology <jessicaleighburnett@gmail.com>
2020-10-17 18:47:33 -0600

    Data updates

--- data-raw/species_list.R
+++ data-raw/species_list.R
@@ -2,7 +2,6 @@
 tempdir<-tempdir()
 
 # AOU --------------------------------------------------------------------
-# Download AOU file to temp folder
 aou.url <- "http://checklist.aou.org/taxa.csv?type=charset%3Dutf-8%3Bsubspecies%3Dno%3B"
 download.file(aou.url, destfile=paste0(tempdir, "aou.csv"))
 # Import AOU
@@ -12,8 +11,12 @@ aou <- aou %>%
     dplyr::mutate(AOU=stringr::str_pad(aou$id, 5, pad="0")) %>% 
     dplyr::select(-id)
 
+# REMOVE FRENCH NAMES BECAUSE SPELL CHECK HATES MY GUTS
+aou <- aou %>% 
+    dplyr::select(-french_name)
 
 # Merge all the lists into a single list ----------------------------------
 species_list <- aou # rihgt now just aou, but leave here for future additions
+
 # Save to /data/ ---------------------------------------------------------------
 usethis::use_data(species_list, overwrite = TRUE)
