e28c78e219666976d494bfcbb07d465ab94ff4db
Jessica Burnett <jburnett@usgs.gov>
2021-07-27 10:46:26 -0600

    Update a message message
    Last one looked more like a warning

--- R/get_bbs_data.R
+++ R/get_bbs_data.R
@@ -39,7 +39,7 @@ if(is.null(sb_id) & !is.null(bbs_version)){
         ind=max(sb_items$release_year)
         sb_id <- sb_items[sb_items$release_year==ind,]$sb_item
         sb_id <- as.character(sb_id)
-        message("FYI: neither `sb_id` nor `bbs_version` were specified. \nDownloading the most recent version of the BBS dataset titled,\n",sbtools::item_get_fields(sb_id,"title"))
+        message("FYI: neither `sb_id` nor `bbs_version` were specified. \n Therefore, I, robot, will download the most recent version of the BBS dataset titled,\n",sbtools::item_get_fields(sb_id,"title"))
     }
 
 # When sb_dir DNE, define it... ------------------------
