576a0e206e4f939a1fc00eae262888e2fe513a14
jburnett <jburnett@usgs.gov>
2021-07-29 07:56:29 -0600

    Updates to most functions.

--- R/unpack_bbs_data.R
+++ R/unpack_bbs_data.R
@@ -1,8 +1,10 @@
-#' @title Decompress the BBS observation files as obtained from ScienceBase.
-#' @param sb_dir ...
+#' @title Decompress the BBS observation files as obtained from ScienceBase. This is an internal function.
+#' @param sb_dir Directory as defined in 
 #' @param country ...
 #' @param state  ...
-#' @param country ...
+#' @param country One of 
+#' 
+#' @keywords internal
 #' 
 #' @export unpack_bbs_data
 
@@ -17,17 +19,16 @@ unpack_bbs_data <- function(
 state_dir <- paste0(sb_dir,"/States")
 # Unzip the States.zip into the sb_id item directory
 unzip(list.files(sb_dir, full.names=TRUE, pattern="States.zip"), exdir = sb_dir)
-# all newly created .zip files....
+# create an object containing all .zip files in the sb_dir
 states.zipped <- list.files(state_dir, full.names=TRUE, ".zip") 
 
 # UNPACK ALL STATES FILES -------------------------------------------------
 # If neither state nor country are specified, just unpack all the state files. 
-if(is.null(state) & is.null(country)){ lapply(states.zipped, unzip, exdir=state_dir)}
+if(is.null(state) & is.null(country)){lapply(states.zipped, unzip, exdir=state_dir)}
 
 
 # UNPACK INDIVIDUAL STATES, OR STATES BY COUNTRY IF SPECIFIED -----------------------------------------------
-# WILL ADD THIS FEATURE SOON!!!!!!
-
+## Feature coming soon.
 
 # END FUNCTION ------------------------------------------------------------
 
