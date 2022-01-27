7eefd19c4f5eefbda2c75389a70695d3beeb4f86
Jessica Burnett <jessicaleighburnett@gmail.com>
2022-01-27 15:45:52 -0700

    Update docs address CMD CHK NOTES
    Alles gut (methinks)

--- R/munge_bbs_data.R
+++ R/munge_bbs_data.R
@@ -1,14 +1,19 @@
 #' @title Subset BBS observations data by one or more species of interest.
 #' @description ...
 #' @param year.range value(s) of year(s) by which to subset the data. If not specified will return all data in the dataset.
-#' @param list A list with element "species_list", obtained from running bbsAssistant::get_bbs_data()...
+#' @param bbs_list A list with element "species_list", obtained from running bbsAssistant::get_bbs_data()...
 #' @param species A vector of one or more species (using English Common Name) to subset the data by. Capitalization ignored.
 #' @param genera vector of one or more genera  to retain in data
+#' @param states vector of one or more states/territorties/sub-national political regions (see data(region_codes))
+#' @param countries vector of one or more nations/countries (see data(region_codes))
 #' @param order  vector of one or more orders to retain in data
 #' @param family vector of one or more families to retain in data
+#' @param QualityCurrentID an index used to indicate the quality of the BBS data. See BBS metadata at USGS ScienceBase for more infromation. Suggest keeping this value at 1 unless you know what you're doing
 #' @param zero.fill If TRUE and a single species is provided in 'species', this function will output list$observations with zero-filled data.
 #' @param active.only Logical. If TRUE keep only active routes. Discontinued routes will be discarded.
 #' @param observations.output return the object as a list of data frames ("list") or a single data frame ("flat" or "df" or "data.frame")
+#' @param keep.stop.level.data logical TRUE will keep the stop-level data and metadata. FALSE will remove these and provide only mean values or sums.
+#' @importFrom dplyr mutate filter across group_by ungroup all_of
 #' @export
 munge_bbs_data <-
   function(bbs_list,
@@ -29,6 +34,8 @@ munge_bbs_data <-
 
 # ARG CHECKS --------------------------------------------------------------
 stopifnot(tolower(observations.output) %in% c("flat", "df","data.table","data.frame", "list"))
+## add binded variables to avoid RCMD CHECK WHINING
+Date <- ObsN <- Active <- AOU <- RTENO <-Year <-iso_3166_2<- iso_a2 <- NULL
 
 # SUBSET BY SPATIAL INDEXES ---------------------------------------------------
 # grab region codes
@@ -41,7 +48,7 @@ if(!is.null(countries)){
 if(!is.null(states)){
   region_codes <- region_codes %>% dplyr::filter(tolower(gsub("-","", iso_3166_2)) %in% tolower(gsub("-","",states)))
 }
-stopifnot(nrow(region_codes)>=1)
+if(nrow(region_codes)<1)stop("Region codes for countries and/or states not found. \nCall `bbsAssistant::region_codes` to ensure you are properly specifying regions.\n")
 
 # filter the bbs data by region_codes
 bbs_list$observations <- bbs_list$observations[ bbs_list$observations$CountryNum %in% region_codes$CountryNum,]
@@ -55,7 +62,7 @@ stopifnot(nrow(bbs_list$observations)>=2)
 # GRAB A LIST OF ALL SAMPLED ROUTES -------------------------------------------------------------------------
 ##before further subsetting, we want to grab an index of which routes were sampled each year (after we subset by year and spatial extent/loc)
 sampled <-  bbs_list$observations %>%
-  distinct(Year, RTENO)
+  dplyr::distinct(Year, RTENO)
 stopifnot(all(sampled$RTENO %in% bbs_list$routes$RTENO))
 
 # TAXONOMIC SUBSETTING ----------------------------------------------------
@@ -74,7 +81,7 @@ if(is.null(species)){species <- unique(bbs_list$observations$AOU)}else{
   }
   stopifnot(!is.null(aous.keep))
   ## now remove the unwanted species from observations
-  bbs_list$observations <- bbs_list$observations %>% filter(AOU %in% aous.keep)
+  bbs_list$observations <- bbs_list$observations %>% dplyr::filter(AOU %in% aous.keep)
 } # end species subsetting
 
 # BBS PROTOCOL SUBSETTING -----------------------------------------------------
@@ -88,13 +95,13 @@ if(active.only) bbs_list$routes  <- bbs_list$routes %>% dplyr::filter(Active==1)
   cols.car <- c(paste0("Car", c(1:50))) # stop-level car covar
 bbs_list$observations <-
   bbs_list$observations %>%
-  mutate(RouteTotal=rowSums(across(all_of(cols.stop))))
+  mutate(RouteTotal=rowSums(dplyr::across(dplyr::all_of(cols.stop))))
 bbs_list$vehicle_data <- bbs_list$vehicle_data %>%
-  mutate(CarMean= round(rowSums(across(all_of(cols.car)))/50)) # rounded
+  mutate(CarMean= round(rowSums(dplyr::across(dplyr::all_of(cols.car)))/50)) # rounded
 bbs_list$vehicle_data <- bbs_list$vehicle_data %>%
-  mutate(NoiseMean= round(rowSums(across(all_of(cols.noise)))/50)) # rounded
+  mutate(NoiseMean= round(rowSums(dplyr::across(dplyr::all_of(cols.noise)))/50)) # rounded
 bbs_list$vehicle_data <- bbs_list$vehicle_data %>%
-  mutate(WindMean= round(rowSums(across(all_of(cols.car)))/50)) # rounded
+  mutate(WindMean= round(rowSums(dplyr::across(dplyr::all_of(cols.car)))/50)) # rounded
 
 # REMOVE STOP.LEVEL DATA (if specified) -----------------------------------
 if(!keep.stop.level.data){
@@ -148,20 +155,19 @@ bbs_list <- lapply(bbs_list, function(x) x <- x[!(tolower(names(x)) %in% "routed
 # CREATE VARIABLE FOR OBSERVER FIRST YEARS --------------------------------
 bbs_list$metadata <- bbs_list$weather %>%
       ##create binary for if observer's first year on the BBS and on the route
-      group_by(ObsN) %>% #observation identifier (number)
-      mutate(ObsFirstYearBBS = ifelse(Date == min(Date), 1, 0)) %>%
-      group_by(ObsN, RTENO, Year) %>%
-      mutate(ObsFirstYearRoute = ifelse(Date == min(Date), 1, 0)) %>%
-      ungroup() # to be safe
-gc()
+  dplyr::group_by(ObsN) %>% #observation identifier (number)
+  dplyr::mutate(ObsFirstYearBBS = ifelse(Date == min(Date), 1, 0)) %>%
+  dplyr::group_by(ObsN, RTENO, Year) %>%
+  dplyr::mutate(ObsFirstYearRoute = ifelse(Date == min(Date), 1, 0)) %>%
+      dplyr::ungroup() # to be safe
 
 # MAKE FLAT DATA OBJECT ---------------------------------------------------
 if(observations.output!="list"){
   cat("Collapsing BBS data and metadata into a single data frame.\n")
-    df <- left_join(bbs_list$observations, bbs_list$vehicle_data)
-    df <- left_join(df, bbs_list$routes)
-    df <- left_join(df, bbs_list$metadata)
-    df <- left_join(df, bbs_list$weather)
+    df <- dplyr::left_join(bbs_list$observations, bbs_list$vehicle_data)
+    df <- dplyr::left_join(df, bbs_list$routes)
+    df <- dplyr::left_join(df, bbs_list$metadata)
+    df <- dplyr::left_join(df, bbs_list$weather)
     bbs_list <- df
     rm(df)
 }
